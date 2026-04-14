6bdd078ed65d367f712cdd697fdc492ebb5230d6
Shamar Morrison <shamar.webdev@gmail.com>
2026-04-13 19:18:40 -0500

    chore: remove accidental console log from business constants

--- lib/business-constants.ts
+++ lib/business-constants.ts
@@ -3,3 +3,5 @@ export const GYM_ADDRESS = '15-17 State Mall, Half Way Tree Road, Kingston, Jama
 export const GYM_CONTACT = '+1 876-919-0009'
 
 export const CARD_FEE_AMOUNT_JMD = 2500
+
+console.log('')
